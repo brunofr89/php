db72a8d1f47b960db6a646bedf71cbb93b2393ca
brunofr89 <brunofr89@hotmail.com>
2020-09-11 21:30:20 -0300

    redirect via php

---
+++ redirect.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Colocar titulo da página</title>
+
+<?
+ Header( 'HTTP/1.1 301 Moved Permanently' );
+ Header( 'Location: https://www.nomedodominio.com.br/' );
+ ?>
+
+</head>
+
+<body>
+</body>
+</html>
